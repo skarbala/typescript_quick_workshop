18c4c96c2c8f05a2d2c6b31336cb00da0c1b81b1
martin <skarbala.martin@gmail.com>
2024-03-21 09:50:55 +0100

    adds description for exercise

--- cypress/e2e/interfaces/exercise.js
+++ cypress/e2e/interfaces/exercise.js
@@ -1,23 +1,27 @@
-import { format, addDays } from "date-fns";
+import { format } from "date-fns";
 
-it('', () => {
+it('working with interfaces', () => {
+    //TODO 1: these 2 objects are bit messy, think about how the structure can be unified
+    //TODO 2: create an interface that would define shape of these 2 objects
     const post_1 = {
-        text: 'This is a very interesting post',
-        socialMedia: SocialMedia.INSTAGRAM,
+        statusText: 'This is a very interesting post',
+        //TODO 3: for social media, create an enum and define fixed social media
+        //more about enums here https://www.typescriptlang.org/docs/handbook/enums.html
+        socialMedia: 'Instagram',
         id: 56,
         scheduledDate: new Date(),
         hashtags: ['nature', 'blog', 'lifestyle']
     }
     const post_2 = {
         text: 'This is a very interesting post',
-        socialMedia: SocialMedia.FACEBOOK,
-        id: 52,
-        scheduledDate: new Date(),
+        socialMedia: 'Facebook',
+        postId: 52,
+        postDate: new Date(),
         hashtags: ['motivation', 'blog', 'lifestyle']
     }
     cy.log(getFormattedDate(post_2))
 })
-
-function getFormattedDate(post) {
-    return format(post.scheduledDate, "d.M.yyyy")
+//TODO 4: update this function = define
+function getFormattedDate(scheduledDate) {
+    return format(scheduledDate, "d.M.yyyy")
 }
