0fc17ef79afb50a7bbf33d91a37e8688eb5e4a32
martin <skarbala.martin@gmail.com>
2024-03-21 09:53:10 +0100

    adds todo for demo

--- cypress/e2e/functions/demo.cy.js
+++ cypress/e2e/functions/demo.cy.js
@@ -1,3 +1,4 @@
+//TODO: define the input parameters types and output of the function
 it('you can put anything into a function', () => {
     cy.log(calculateViral(1500, 12))
     cy.log(calculateViral('1500k', 15))
